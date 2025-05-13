ba14b183c2375517a25e1f7eec26b06c0705a2f2
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2025-05-13 18:35:27 -0300

    refactor: numera apropriadamente a versão do plugin.

--- version.php
+++ version.php
@@ -4,6 +4,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_course_rating';
 $plugin->release = '1.0';
-$plugin->version = 202502141803;
+$plugin->version = 2025_02_14_18_03;
 $plugin->requires = 2021051700;
 $plugin->maturity = MATURITY_RC;
